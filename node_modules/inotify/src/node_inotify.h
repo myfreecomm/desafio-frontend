c436139fa4e5b73cccc2bfec7a57cfb4d63c9b42
paulo <paulofrontend@gmail.com>
2017-10-30 23:40:01 -0200

    creating config project and structure folder and html css

---
+++ node_modules/inotify/src/node_inotify.h
@@ -0,0 +1,25 @@
+// Copyright 2010, Camilo Aguilar. Cloudescape, LLC.
+#ifndef SRC_NODE_INOTIFY_H_
+#define SRC_NODE_INOTIFY_H_
+
+#include <node.h>
+#include <node_version.h>
+#include <node_object_wrap.h>
+#include <sys/inotify.h>
+#include <sys/select.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "nan.h"
+#define NODE_INOTIFY_VERSION "1.3.0"
+
+using namespace v8;
+using namespace node;
+
+namespace NodeInotify {
+
+}
+
+#endif  // SRC_NODE_INOTIFY_H_
+
