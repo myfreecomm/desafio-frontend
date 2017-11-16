efed07920b01a25e6786e66e94bb0e85eee92ac3
Lucas <lucassantosamaral@hotmail.com>
2017-11-16 00:33:54 -0200

    menu com scroll

---
+++ scroll.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    
+    offset = $('.theme-features').offset().top;
+
+    $(document).scroll(function () {
+        if ((offset - 200) <= $(window).scrollTop()) {
+            $('header').addClass('fixed');
+        } else {
+            $('header').removeClass('fixed');
+        }
+    });
+    
+});
